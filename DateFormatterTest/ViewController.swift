7b8ddab436fee307ea65abc38d1787fedd7ab25a
Maxim Mikheev <maxim@keyflow.se>
2015-12-01 10:10:43 +0100

    Add date parsing logic. Remove unused code.

--- DateFormatterTest/ViewController.swift
+++ DateFormatterTest/ViewController.swift
@@ -1,25 +1,49 @@
-//
-//  ViewController.swift
-//  DateFormatterTest
-//
-//  Created by Maksim Mikheev on 01/12/15.
-//  Copyright © 2015 Maksim Mikheev. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        parse()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func parse() {
+        let date1 = "2015-12-16T23:00:00+00:00"
+        let date2 = "2015-12-16T23:00:00+01:00"
+        
+        // Works for 24-Hour time format, doesn't work for 12-Hour format.
+        // To enable/disable 24-Hour time format go to Settings -> General -> Date & Time -> 24-Hour Time.
+        parseDate(date1, false)
+        parseDate(date2, false)
+        
+        // This works for both 12/24-Hour time format.
+        parseDate(date1, true)
+        parseDate(date2, true)
+        
+        /* 24-Hour time enabled
+        Parsed date '2015-12-16T23:00:00+00:00' into '2015-12-16 23:00:00 +0000'
+        Parsed date '2015-12-16T23:00:00+01:00' into '2015-12-16 22:00:00 +0000'
+        Parsed date '2015-12-16T23:00:00+00:00' into '2015-12-16 23:00:00 +0000'
+        Parsed date '2015-12-16T23:00:00+01:00' into '2015-12-16 22:00:00 +0000'
+        */
+        
+        /* 24-Hour time disabled
+        Can't parse date '2015-12-16T23:00:00+00:00'
+        Can't parse date '2015-12-16T23:00:00+01:00'
+        Parsed date '2015-12-16T23:00:00+00:00' into '2015-12-16 11:00:00 PM +0000'
+        Parsed date '2015-12-16T23:00:00+01:00' into '2015-12-16 10:00:00 PM +0000'
+        */
     }
-
-
+    
+    func parseDate(dateString: String, _ enforceEnUSPOSIXLocale: Bool) {
+        let formatter = NSDateFormatter()
+        if enforceEnUSPOSIXLocale { formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX") }
+        formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZZ'"
+        
+        if let date = formatter.dateFromString(dateString) {
+            print("Parsed date '\(dateString)' into '\(date)'")
+        } else {
+            print("Can't parse date '\(dateString)'")
+        }
+    }
 }
-
